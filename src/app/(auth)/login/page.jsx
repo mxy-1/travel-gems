329ce7ddfe2faa9306c6890e796902a6e7083d06
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-01-28 19:28:56 +0000

    remove github login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -3,13 +3,13 @@ import styles from "./login.module.css"
 import { handleGithubLogin } from "@/lib/action";
 
 const LoginPage = () => {
-    
+    //  issue with user page and github log in
     return (
         <main className={styles.container}>
             <div className={styles.wrapper}>
-                <form action={handleGithubLogin}>
+                {/* <form action={handleGithubLogin}>
                     <button className={styles.github}>Login with GitHub</button>
-                </form>
+                </form> */}
                 <LoginForm />
             </div>
         </main>
