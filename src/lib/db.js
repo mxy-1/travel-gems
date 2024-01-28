b85bb96dce997d4632ac981be297c8928f2b1e6e
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-01-28 20:11:54 +0000

    refactor

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,40 +1,51 @@
 import mongoose from "mongoose";
 
-global.mongoose = {
-  conn: null,
-  promise: null
-}
-
-const getMongoUri = () => {
-  if (process.env.NODE_ENV === 'production') return process.env.MONGODB_URI;
-  if (process.env.NODE_ENV === 'test') return process.env.MONGODB_URI_TEST;
-  return process.env.MONGODB_URI_DEV;
-};
-
 export const connectToDatabase = async () => {
-  let promise;
   try {
-    if (global.mongoose && global.mongoose.conn) {
-      console.log('connected from previous')
-      return global.mongoose.conn;
-    } else {
-      promise = mongoose.connect(getMongoUri(), {
-        autoIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(typeof promise, "type???")
-      console.log(promise, "The promise")
-    }
-
-    global.mongoose = {
-      conn: await promise,
-      promise,
-    }
-    console.log("Newly connected to MongoDB")
-
+    await mongoose.connect(process.env.MONGODB_URI_DEV)
+    console.log("connected to mongodb")
+    
   } catch (error) {
     console.log(error)
-    throw new Error("Error connecting to the database: ", error)
   }
 }
+
+
+// global.mongoose = {
+//   conn: null,
+//   promise: null
+// }
+
+// const getMongoUri = () => {
+//   if (process.env.NODE_ENV === 'production') return process.env.MONGODB_URI;
+//   if (process.env.NODE_ENV === 'test') return process.env.MONGODB_URI_TEST;
+//   return process.env.MONGODB_URI_DEV;
+// };
+
+// export const connectToDatabase = async () => {
+//   let promise;
+//   try {
+//     if (global.mongoose && global.mongoose.conn) {
+//       console.log('connected from previous')
+//       return global.mongoose.conn;
+//     } else {
+//       promise = mongoose.connect(getMongoUri(), {
+//         autoIndex: true,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+//       console.log(typeof promise, "type???")
+//       console.log(promise, "The promise")
+//     }
+
+//     global.mongoose = {
+//       conn: await promise,
+//       promise,
+//     }
+//     console.log("Newly connected to MongoDB")
+
+//   } catch (error) {
+//     console.log(error)
+//     throw new Error("Error connecting to the database: ", error)
+//   }
+// }
